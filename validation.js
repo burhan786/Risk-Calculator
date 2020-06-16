282c7a2f6b68ff1cc33315ad5d2ebb5f0ee5a987
burhan786 <rangwalaburhanuddin56@gmail.com>
2020-06-16 22:57:46 +0530

    Added changes

--- validation.js
+++ validation.js
@@ -212,9 +212,9 @@ function checkParam(){
     if(age.length != 0 && pd != -1 && ckd != -1 && dm != -1 && htn != -1 && hiv != -1 && imt != -1 && rr.length != 0 && hr.length != 0 && sp.length != 0 && dd.length != 0 && cpk.length != 0 && crp.length != 0 && ldh.length != 0 && tr.length != 0 && fr.length != 0 && alc.length != 0){
         if(count_epidem > 0 && count_vital > 0 && count_lab > 0){
            var modal = document.getElementById("myModal");
-           document.getElementById("span1").innerHTML = count_epidem + "Parameters Crossed Limit";
-           document.getElementById("span2").innerHTML = count_vital + "Parameters Crossed Limit";
-           document.getElementById("span3").innerHTML = count_lab + "Parameters Crossed Limit";
+           document.getElementById("span1").innerHTML = count_epidem +" "+" "+ "Parameters Crossed Limit";
+           document.getElementById("span2").innerHTML = count_vital +" "+" "+ "Parameters Crossed Limit";
+           document.getElementById("span3").innerHTML = count_lab +" "+" "+ "Parameters Crossed Limit";
            var total = count_epidem+count_lab+count_lab;
            console.log("The total number of parameters are high risk:" + total);
            document.getElementById("risk_fact").innerHTML = "Risk Factor is " + total;
@@ -230,12 +230,12 @@ function checkParam(){
            }
         }else{
             var modal1 = document.getElementById("myModal1");
-            document.getElementById("sspan1").innerHTML = count_epidem + "Parameters Crossed Limit";
-            document.getElementById("sspan2").innerHTML = count_vital + "Parameters Crossed Limit";
-            document.getElementById("sspan3").innerHTML = count_lab + "Parameters Crossed Limit";
+            document.getElementById("sspan1").innerHTML = count_epidem + " "+" "+"Parameters Crossed Limit";
+            document.getElementById("sspan2").innerHTML = count_vital +" "+" " +"Parameters Crossed Limit";
+            document.getElementById("sspan3").innerHTML = count_lab +" "+ " "+ "Parameters Crossed Limit";
             var total = count_epidem+count_lab+count_lab;
             console.log("The total number of parameters are low risk:" + total);
-            document.getElementById("risk_fact").innerHTML = "Risk Factor is " + total;
+            document.getElementById("rissk_fact").innerHTML = "Risk Factor is " + total;
             modal1.style.display = "block";
             var sspan1 = document.getElementsByClassName("close1")[0];
             sspan1.onclick = function(){
